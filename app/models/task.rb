28c74e3aa27dbfedf6525c0d17579254bf790a55
skydragon <bwalikurunda@gmail.com>
2021-10-19 20:49:13 +0100

    finish the styling

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,5 +10,28 @@ class Task < ApplicationRecord
 
   ] 
 
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def complete?
+    status == 'complete'
+  end
+
+  def in_progress?
+    status == 'in-progress'
+  end
+
+  def not_started?
+    status == 'not-started'
+  end
+
 
 end
